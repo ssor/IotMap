760c99ad937ccb3f67b5a0b15f76d684a0f63e0a
ssor <ssor@qq.com>
2013-08-05 15:19:47 +0800

    支持手机端实时图片展示

--- Lib/Action/MobileAction.class.php
+++ Lib/Action/MobileAction.class.php
@@ -27,6 +27,79 @@ class MobileAction extends Action{
 		$this->display();
 		
 	}
+	public function index_realtime_camera()
+	{
+		$id = Tools::request("id");
+		$camera_text = $this->get_camera_text_by_id($id);
+
+		$this->assign("pic_name","rt");
+		$this->assign("camera_id", $id);
+		$this->assign("camera_text", $camera_text);
+		// $this->assign("pic_name","Blender");
+		$this->display();
+	}
+	public function get_new_pic()
+	{
+		$id = Tools::request("id");
+		$finded = false;
+		// echo stristr("1001-rt.png", $id); return;
+		// echo $id;return;
+		foreach(glob("Public/realTimePic/*") as $d)
+		{
+			//echo $d;echo "<br>";
+			///*
+		    if(!is_dir($d))
+		    {
+		    	$name = basename($d);
+		        if($name != 'rt.png')
+		        {
+					//echo $name;echo "<br>";
+					///*
+		        	// $bfind = stripos($name, $id, 0);
+					if(false === strpos($name, $id)){
+		        	//if(false == stristr($name, $id, 0)){
+						//echo $name.' dose not contin id '.$id.'<br>';
+		        		//没找到
+		        		continue;
+		        	}
+		        	else{
+		        		$finded = true;
+				        echo $name;
+				        break;
+		        	}
+					//*/
+		        }
+		    }
+			//*/
+		}
+		if($finded == false){
+			echo "rt.png";
+		}
+		// echo "Blender";
+		// echo "rt2";
+	}
+	function get_camera_text_by_id($id)
+	{
+		$camera_list = $this->camera_list();
+        if (!empty($id)) {
+            foreach ($camera_list as $key => $value) {
+                if($value['id'] == $id)
+                {
+                    return $value['text'];
+                }
+            }
+        }
+        else
+            return false;
+	}
+	function camera_list()
+	{
+		$list = array();
+		$list[] = array('id'=>'10001', 'text'=>'环境科学楼');
+		$list[] = array('id'=>'10002', 'text'=>'科技楼');
+		$list[] = array('id'=>'10003', 'text'=>'学一宿舍楼');
+		return $list;
+	}
 }
 
 ?>
